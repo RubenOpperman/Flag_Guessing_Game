ac8858ee60fe6274fb2f7ad275c931839be3a6c5
Ruben Opperman <Oppiesfam123@gmail.com>
2026-04-21 13:07:39 +0200

    display flag

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -2,16 +2,9 @@ export async function fetchData(url) {
   try {
     const response = await fetch(url);
 
-    if (!response.ok) {
-      throw new Error("HTTP error! status: ${response.status}");
-    }
-
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
     console.error("oopsies", error);
   }
 }
-
-return fetchData("https://flagfeed.com");
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,19 @@
 import { fetchData } from "./api.js";
+const data = await fetchData(
+  "https://restcountries.com/v3.1/all?fields=name,capital,currencies,flags",
+);
+
+Math.random();
+
+// fetching detailed country information
+const officialNames = data.map((country) => country.name.official);
+const imgSrc = data.map((countries) => countries.flags.png);
+const imgAlt = data.map((countries) => countries.flags.alt);
 
 const img_container = document.getElementById("imageContainer");
 const img = document.createElement("img");
 
-img.src = "../images/ID_Photo_Clarissa_Opperman.jpg";
-img.alt = "South Africa Flag";
+img.src = imgSrc[Math.floor(Math.random() * data.length)];
+img.alt = imgAlt[Math.floor(Math.random() * data.length)];
 
 img_container.appendChild(img);
-console.log(fetchData());
